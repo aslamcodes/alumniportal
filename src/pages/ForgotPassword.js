677f13ea33d8dd879f650da0b50cd9b9754de882
jayvanandel07 <j.vanandel07@gmail.com>
2022-06-07 11:43:40 +0530

    verification page

--- src/pages/ForgotPassword.js
+++ src/pages/ForgotPassword.js
@@ -1,32 +1,100 @@
-import React from "react";
+import React, { useState } from "react";
 import styles from "./ForgotPassword.module.css";
 import hypocrisy from "../assets/forgotPassword.png";
+import verification from "../assets/verification.png";
 import { Link } from "react-router-dom";
 const ForgotPassword = () => {
+  const [form, setForm] = useState(1);
+  const [data, setData] = useState({
+    email: "",
+    phoneNo: "",
+    otp: "",
+  });
+
+  const handleChange = (e) => {
+    e.preventDefault();
+    setData({
+      ...data,
+      [e.target.name]: e.target.value,
+    });
+  }
+
+
+
   return (
     <div className={styles.container}>
       <div className={styles.image_container}>
-        <img src={hypocrisy} alt="register image" />
+        <img src={
+          form === 1 && hypocrisy || form === 2 && verification || form === 3 && hypocrisy
+        } alt="forgot password image" />
       </div>
       <div className={styles.form_container}>
         <div className={styles.form}>
           <div className={styles.form_header}>
-            <h1>Forgot Password</h1>
+            <h1>{
+              form === 1 && "Forgot Password" ||
+              form === 2 && "Verification" ||
+              form === 3 && "Change password"
+            }</h1>
           </div>
           <div className={styles.form_body}>
-            <form >
-              <div className={styles.form_input_container}>
-                <input type="text" placeholder="Enter your email ID" />
-              </div>
-              <p>Or</p>
-              <div className={styles.form_input_container}>
-                <input type="text" placeholder="Enter your Phone number" />
-              </div>
-              <div className={styles.form_actions_container}>
-                <Link to='/login'>Back to Login</Link>
-                <button >Next</button>
-              </div>
-            </form>
+            {form === 1 && (
+              // form 1
+              < form >
+                <div className={styles.form_input_container}>
+                  <input name="email" id="email" type="text" placeholder="Enter your email ID" value={data.email} onChange={handleChange} />
+                </div>
+                <p>Or</p>
+                <div className={styles.form_input_container}>
+                  <input name="phoneNo" id="phoneNo" type="text" placeholder="Enter your Phone number" value={data.phoneNo} onChange={handleChange} />
+                </div>
+                <div className={styles.form_actions_container}>
+                  <Link to='/login'>Back to Login</Link>
+                  <button onClick={() => setForm(2)}>Next</button>
+                </div>
+              </form>
+            )}
+
+            {form === 2 && (
+              // form 2
+              <form
+                style={{ "align-items": 'center' }}
+              >
+                <div className={styles.form_input_container}>
+                  <input name="otp" id="otp" type="number" placeholder="Enter the OTP" value={data.otp} onChange={handleChange} />
+                </div>
+                <div className={styles.form_vactions_container}>
+
+                  <p>
+                    Didn't recieve code?{" "}
+                    {/* <Link to="/register"> */}
+                    <span>Request Again</span>
+                    {/* </Link> */}
+                  </p>
+                  <p onClick={() => setForm(1)}>Change Phone Number/Email ID</p>
+                </div>
+                <div className={styles.form_actions_container}>
+                  <button >Change password</button>
+                </div>
+              </form>
+            )}
+
+            {form === 3 && (
+              // form 3
+              < form >
+                <div className={styles.form_input_container}>
+                  <input name="email" id="email" type="text" placeholder="Enter your email ID" value={data.email} onChange={handleChange} />
+                </div>
+                <p>Or</p>
+                <div className={styles.form_input_container}>
+                  <input name="phoneNo" id="phoneNo" type="text" placeholder="Enter your Phone number" value={data.phoneNo} onChange={handleChange} />
+                </div>
+                <div className={styles.form_actions_container}>
+                  <Link to='/login'>Back to Login</Link>
+                  <button >Next</button>
+                </div>
+              </form>
+            )}
 
           </div>
           <hr />
@@ -40,7 +108,7 @@ const ForgotPassword = () => {
           </div>
         </div>
       </div>
-    </div>
+    </div >
   )
 };
 
