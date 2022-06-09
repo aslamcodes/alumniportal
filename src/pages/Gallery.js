8d6b20a810557bc08b24a6a8893e0a0be8d3025d
jayvanandel07 <j.vanandel07@gmail.com>
2022-06-09 12:05:13 +0530

    temp

--- src/pages/Gallery.js
+++ src/pages/Gallery.js
@@ -4,39 +4,42 @@ const Gallery = () => {
   return (
     <div className={styles.gallery}>
       <h1>Gallery</h1>
-      <hr />
-      <div className={styles.galleryItems}>
-        <div className={styles.galleryItem}>
-          <div className={styles.itemImg}>
-            <img src="https://picsum.photos/id/1/200/300" alt="gallery" />
+      <div className={styles.gallery_container}>
+        <div className={styles.gallery_items_container}>
+          <div className={styles.gallery_item_container}>
+            <div className={styles.gallery_item}>
+              <div className={styles.itemImg}>
+                <img src="https://picsum.photos/id/1/200/300" alt="gallery" />
+              </div>
+              <div className={styles.itemTitle}>
+                <h2>ALUMNI <span>MEET</span></h2>
+              </div>
+            </div>
           </div>
-          <hr />
-          <div className={styles.itemTitle}>
-            <h2>ALUMNI <span>MEET</span></h2>
+          <div className={styles.gallery_item_container}>
+            <div className={styles.gallery_item}>
+              <div className={styles.itemTitle}>
+                <h2><span>ALL</span> PHOTOS</h2>
+              </div>
+
+              <div className={styles.itemImg}>
+                <img src="https://picsum.photos/id/2/200/300" alt="gallery" />
+              </div>
+            </div>
           </div>
-        </div>
-        <hr />
-        <div className={styles.galleryItem}>
-          <div className={styles.itemTitle}>
-            <h2><span>ALL</span> PHOTOS</h2>
-          </div>
-          <hr />
-          <div className={styles.itemImg}>
-            <img src="https://picsum.photos/id/2/200/300" alt="gallery" />
-          </div>
-        </div>
-        <hr />
-        <div className={styles.galleryItem}>
-          <div className={styles.itemTitle}>
-            <h2><span>SEMINAR</span> SESSIONS</h2>
-          </div>
-          <hr />
-          <div className={styles.itemImg}>
-            <img src="https://picsum.photos/id/3/200/300" alt="gallery" />
+          <div className={styles.gallery_item_container}>
+            <div className={styles.gallery_item}>
+              <div className={styles.itemTitle}>
+                <h2><span>SEMINAR</span> SESSIONS</h2>
+              </div>
+
+              <div className={styles.itemImg}>
+                <img src="https://picsum.photos/id/3/200/300" alt="gallery" />
+              </div>
+            </div>
           </div>
         </div>
       </div>
-      <hr />
     </div>
   );
 };
