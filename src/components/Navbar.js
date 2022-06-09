8ee2176250df1f090afa22bb11979d6358b80316
jayvanandel07 <j.vanandel07@gmail.com>
2022-06-09 16:17:11 +0530

    fix the blur in navbar and menu

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,8 +1,10 @@
 import React, { useState, useEffect } from 'react';
 import MenuIcon from '@mui/icons-material/Menu';
-import CloseIcon from '@mui/icons-material/Close';
+
 import styles from "./Navbar.module.css";
 import { Link } from 'react-router-dom';
+
+import Menu from "./Menu";
 import { useWindowScrollPositions } from './useWindowScrollPositions';
 
 function getWindowDimensions() {
@@ -39,67 +41,64 @@ const Navbar = () => {
 
 
   return (
-    <div className={`${styles.navbar} ${!menuActive && styles.background_blur} ${isScrolled && styles.scrolled}`}>
-      {windowDimensions.width > 790 &&
-        <div className={`${styles.navLink}`} >
-          <Link to="/">Home</Link>
-          <Link to="/gallery">Gallery</Link>
-          <Link to="/events">Events</Link>
-        </div>
-      }
-
-      {windowDimensions.width > 790 ?
-        <div className={styles["navbar-brand"]}>
-          <div className={styles["navbar-logo"]}>
-            <img src={require("../assets/Logo1.png")} alt="SKI logo" />
-
+    <div className={styles.container}>
+      <div className={`${styles.navbar} ${styles.background_blur} ${isScrolled && styles.scrolled}`}>
+        {windowDimensions.width > 790 &&
+          <div className={`${styles.navLink}`} >
+            <Link to="/">Home</Link>
+            <Link to="/gallery">Gallery</Link>
+            <Link to="/events">Events</Link>
           </div>
-          <div className={styles["titleText"]}>
-            <h1>SRI KRISHNA COLLEGE OF TECHNOLOGY</h1>
-            <h3>AUTONOMOUS INSTITUTION | ACCREDITED BY NAAC WITH ‘A’ GRADE</h3>
-          </div>
-          <div className={styles["navbar-logo"]}>
-            <img src={require("../assets/Logo2.png")} alt="SKCT logo" />
-          </div>
-        </div> :
-        <div className={styles["navbar-brand"]}>
+        }
 
-          {windowDimensions.width > 350 &&
+        {windowDimensions.width > 790 ?
+          <div className={styles["navbar-brand"]}>
+            <div className={styles["navbar-logo"]}>
+              <img src={require("../assets/Logo1.png")} alt="SKI logo" />
+
+            </div>
+            <div className={styles["titleText"]}>
+              <h1>SRI KRISHNA COLLEGE OF TECHNOLOGY</h1>
+              <h3>AUTONOMOUS INSTITUTION | ACCREDITED BY NAAC WITH ‘A’ GRADE</h3>
+            </div>
             <div className={styles["navbar-logo"]}>
               <img src={require("../assets/Logo2.png")} alt="SKCT logo" />
-              <hr />
             </div>
-          }
-          <div className={styles["titleText"]}>
-            <p>SKCT</p>
-          </div>
-        </div>
-      }
-      {windowDimensions.width > 790 ?
-        <div className={`${styles.navLink} ${styles.right}`}>
-          <Link to="/alumini-forum">Alumini Forum</Link>
-          <Link to="/office-bearers">Office Bearers</Link>
-        </div> :
-        <div className={`${styles.dropdown} }`}>
-          {!menuActive &&
-            <MenuIcon className={styles["dropdown-btn"]} onClick={() => { setMenuActive(true) }} />
-          }
-          {menuActive &&
-            <div className={`${styles.dropdownContainer} ${menuActive && styles.background_blur}`} >
-              <CloseIcon className={styles["dropdown-close"]} onClick={() => { setMenuActive(false) }} />
-              <div className={`${styles.navLink}`} >
-                <Link onClick={() => setMenuActive(false)} to="/">Home</Link>
-                <Link onClick={() => setMenuActive(false)} to="/gallery">Gallery</Link>
-                <Link onClick={() => setMenuActive(false)} to="/events">Events</Link>
-                <Link onClick={() => setMenuActive(false)} to="/alumini-forum">Alumini Forum</Link>
-                <Link onClick={() => setMenuActive(false)} to="/office-bearers">Office Bearers</Link>
+          </div> :
+          <div className={styles["navbar-brand"]}>
+
+            {windowDimensions.width > 350 &&
+              <div className={styles["navbar-logo"]}>
+                <img src={require("../assets/Logo2.png")} alt="SKCT logo" />
+                <hr />
               </div>
+            }
+            <div className={styles["titleText"]}>
+              <p>SKCT</p>
             </div>
-          }
-        </div>
+          </div>
+        }
+        {windowDimensions.width > 790 ?
+          <div className={`${styles.navLink} ${styles.right}`}>
+            <Link to="/alumini-forum">Alumini Forum</Link>
+            <Link to="/office-bearers">Office Bearers</Link>
+          </div> :
+          <div className={`${styles.dropdown} }`}>
+            {!menuActive &&
+              <MenuIcon className={styles["dropdown-btn"]} onClick={() => { setMenuActive(true) }} />
+            }
+          </div>
+        }
+
+
+      </div >
+
+      {menuActive && windowDimensions.width < 790 &&
+        <Menu setMenuActive={setMenuActive} />
       }
 
-    </div >
+    </div>
+
   )
 }
 
