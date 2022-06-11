58c06e4b4a1f5ac1e716ea28564487af8d633dd8
Mohamed Aslam <mohamedaslammn.2003@gmail.com>
2022-06-11 18:48:12 +0530

    Pages Folder Organization

--- src/pages/AlumniForum/AlumniForum.js
+++ src/pages/AlumniForum/AlumniForum.js
@@ -35,7 +35,7 @@ const DUMMY_POST_DATA = [
       {
         user: {
           name: "Enrico",
-          profile_image: require("./../assets/ben.png"),
+          profile_image: require("assets/ben.png"),
         },
         comment:
           "Lorem Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.",
@@ -75,7 +75,7 @@ const DUMMY_POST_DATA = [
       {
         user: {
           name: "Enrico",
-          profile_image: require("./../assets/ben.png"),
+          profile_image: require("assets/ben.png"),
         },
         comment:
           "Lorem Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.",
@@ -118,7 +118,7 @@ const DUMMY_POST_DATA = [
     id: 2,
     user: {
       name: "Ben Tennyson",
-      profile_image: require("./../assets/ben.png"),
+      profile_image: require("assets/ben.png"),
     },
     post: {
       images: [
@@ -134,7 +134,7 @@ const DUMMY_POST_DATA = [
       {
         user: {
           name: "name",
-          profile_image: require("./../assets/ben.png"),
+          profile_image: require("assets/ben.png"),
         },
         comment: "Hello Ben",
         replies: [{ user_name: "Mr. Sudalai", text: "Hello" }],
@@ -142,7 +142,7 @@ const DUMMY_POST_DATA = [
       {
         user: {
           name: "name",
-          profile_image: require("./../assets/ben.png"),
+          profile_image: require("assets/ben.png"),
         },
         comment: "Hi Ben",
         replies: ["Hello"],
---
+++ src/pages/index.js
@@ -0,0 +1,21 @@
+import Admin from "pages/Admin/Admin";
+import AlumniForum from "pages/AlumniForum/AlumniForum";
+import RegistrationPage from "./RegisterationPage/RegistrationPage";
+import GalleryPage from "./Gallery/Gallery";
+import OfficeBearers from "./OfficeBearers/OfficeBearers";
+import Login from "./Login/LoginPage";
+import Home from "./Home/Home";
+import Events from "./Events/Events";
+import ForgotPassword from "./ForgotPassword/ForgotPassword";
+
+export {
+  Events,
+  Home,
+  Admin,
+  AlumniForum,
+  RegistrationPage,
+  GalleryPage,
+  OfficeBearers,
+  Login,
+  ForgotPassword,
+};
