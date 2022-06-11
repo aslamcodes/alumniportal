17a71d8f313f29846adfe4b57991f2830f66e56a
jayvanandel07 <j.vanandel07@gmail.com>
2022-06-11 16:18:20 +0530

    new post modal done

---
+++ src/components/Modal/NewPostModal.js
@@ -0,0 +1,64 @@
+
+import React, { useState } from 'react';
+import ReactPortal from "../ReactPortal";
+import { IoClose } from "react-icons/io5";
+import { BiImageAdd } from "react-icons/bi";
+import styles from './NewPostModal.module.css'
+
+function NewPostModal({ data, setNewPostActive }) {
+  const [image, setImage] = useState(null);
+  const onSelectFile = e => {
+    if (!e.target.files || e.target.files.length === 0) {
+      setImage(undefined)
+      return
+    }
+    // I've kept this example simple by using the first image instead of multiple
+    setImage(e.target.files[0])
+  }
+  return (
+    <ReactPortal wrapperId="new_post_modal_wrapper">
+      <div className={styles.new_post_overlay}>
+        <div className={styles.new_post_container}>
+          <div className={styles.header}>
+            <div className={styles.userinfo_container}>
+              <img src={data.user.profile_image} />
+              <p className={styles.user_name}>{data.user.name}</p>
+            </div>
+            <IoClose size="30px" className={styles.close_icon} onClick={() => setNewPostActive(false)} />
+          </div>
+          <div className={styles.body}>
+            <form action="">
+              <div className={styles.input_container}>
+                <input type="text" id="cname" placeholder="Company Name" />
+              </div>
+              <div className={styles.input_container}>
+                <input type="text" id="designation" placeholder="Designation" />
+              </div>
+              <div className={styles.description_container}>
+                <div className={styles.input_container}>
+                  <input type="text" id="title" placeholder="title" />
+                </div>
+                <div className={styles.input_container}>
+                  <textarea type="text" id="description" placeholder="description" />
+                </div>
+              </div>
+              <div className={styles.img_input_container}>
+                {image ? <img src={URL.createObjectURL(image)} /> :
+                  <div className={styles.img_input}>
+                    <label for="img-input">
+                      <BiImageAdd size="50px" />
+                      <p>Add Image</p>
+                    </label>
+                    <input id="img-input" type="file" onChange={onSelectFile} />
+                  </div>
+                }
+              </div>
+            </form>
+          </div>
+        </div>
+      </div>
+    </ReactPortal>
+  )
+}
+
+export default NewPostModal
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -43,69 +43,69 @@ const Navbar = () => {
   }, []);
 
   return (
-
-    <div className={styles.container}>
-      <div className={`${styles.navbar} ${styles.background_blur} ${isScrolled && styles.scrolled}`}>
-        {windowDimensions.width > 790 &&
-          <div className={`${styles.navLink}`} >
-            <Link to="/">Home</Link>
-            <Link to="/gallery">Gallery</Link>
-            <Link to="/events">Events</Link>
-          </div>
-        }
-
-        {windowDimensions.width > 790 ?
-          <div className={styles["navbar-brand"]}>
-            <div className={styles["navbar-logo"]}>
-              <img src={require("../assets/Logo1.png")} alt="SKI logo" />
-
+    isNavbarVisible && (
+      <div className={styles.container}>
+        <div className={`${styles.navbar} ${styles.background_blur} ${isScrolled && styles.scrolled}`}>
+          {windowDimensions.width > 790 &&
+            <div className={`${styles.navLink}`} >
+              <Link to="/">Home</Link>
+              <Link to="/gallery">Gallery</Link>
+              <Link to="/events">Events</Link>
             </div>
-            <div className={styles["titleText"]}>
-              <h1>SRI KRISHNA COLLEGE OF TECHNOLOGY</h1>
-              <h3>AUTONOMOUS INSTITUTION | ACCREDITED BY NAAC WITH ‘A’ GRADE</h3>
-            </div>
+          }
 
-            <div className={styles["navbar-logo"]}>
-              <img src={require("../assets/Logo2.png")} alt="SKCT logo" />
-            </div>
-
-          </div> :
-          <div className={styles["navbar-brand"]}>
-
-            {windowDimensions.width > 350 &&
+          {windowDimensions.width > 790 ?
+            <div className={styles["navbar-brand"]}>
               <div className={styles["navbar-logo"]}>
-                <img src={require("../assets/Logo2.png")} alt="SKCT logo" />
-                <hr />
+                <img src={require("../assets/Logo1.png")} alt="SKI logo" />
 
               </div>
-            }
-            <div className={styles["titleText"]}>
-              <p>SKCT</p>
+              <div className={styles["titleText"]}>
+                <h1>SRI KRISHNA COLLEGE OF TECHNOLOGY</h1>
+                <h3>AUTONOMOUS INSTITUTION | ACCREDITED BY NAAC WITH ‘A’ GRADE</h3>
+              </div>
+
+              <div className={styles["navbar-logo"]}>
+                <img src={require("../assets/Logo2.png")} alt="SKCT logo" />
+              </div>
+
+            </div> :
+            <div className={styles["navbar-brand"]}>
+
+              {windowDimensions.width > 350 &&
+                <div className={styles["navbar-logo"]}>
+                  <img src={require("../assets/Logo2.png")} alt="SKCT logo" />
+                  <hr />
+
+                </div>
+              }
+              <div className={styles["titleText"]}>
+                <p>SKCT</p>
+              </div>
+
             </div>
+          }
+          {windowDimensions.width > 790 ?
+            <div className={`${styles.navLink} ${styles.right}`}>
+              <Link to="/alumni-forum">Alumni Forum</Link>
+              <Link to="/office-bearers">Office Bearers</Link>
+            </div> :
+            <div className={`${styles.dropdown} }`}>
+              {!menuActive &&
+                <MenuIcon className={styles["dropdown-btn"]} onClick={() => { setMenuActive(true) }} />
+              }
+            </div>
+          }
 
-          </div>
+
+        </div >
+
+        {menuActive && windowDimensions.width < 790 &&
+          <Menu setMenuActive={setMenuActive} />
         }
-        {windowDimensions.width > 790 ?
-          <div className={`${styles.navLink} ${styles.right}`}>
-            <Link to="/alumini-forum">Alumini Forum</Link>
-            <Link to="/office-bearers">Office Bearers</Link>
-          </div> :
-          <div className={`${styles.dropdown} }`}>
-            {!menuActive &&
-              <MenuIcon className={styles["dropdown-btn"]} onClick={() => { setMenuActive(true) }} />
-            }
-          </div>
-        }
 
-
-      </div >
-
-      {menuActive && windowDimensions.width < 790 &&
-        <Menu setMenuActive={setMenuActive} />
-      }
-
-    </div>
-
+      </div>
+    )
   )
 }
 
