fd605d592abba12d1c66287b826361aa9a53b1f2
Mohamed Aslam <mohamedaslammn.2003@gmail.com>
2022-06-11 12:01:21 +0530

    Conflick Fix

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,13 +1,11 @@
-
-import React, { useState, useEffect } from 'react';
-import MenuIcon from '@mui/icons-material/Menu';
+import React, { useState, useEffect } from "react";
+import MenuIcon from "@mui/icons-material/Menu";
 
 import styles from "./Navbar.module.css";
-import { Link } from 'react-router-dom';
+import { Link } from "react-router-dom";
 
 import Menu from "./Menu";
-import { useWindowScrollPositions } from './useWindowScrollPositions';
-
+import { useWindowScrollPositions } from "./useWindowScrollPositions";
 
 function getWindowDimensions() {
   const { innerWidth: width, innerHeight: height } = window;
@@ -43,7 +41,6 @@ const Navbar = () => {
   }, []);
 
   return (
-<<<<<<< HEAD
     isNavbarVisible && (
       <div
         className={`${styles.navbar} ${!menuActive && styles.background_blur} ${
@@ -150,72 +147,5 @@ const Navbar = () => {
     )
   );
 };
-=======
-
-    <div className={styles.container}>
-      <div className={`${styles.navbar} ${styles.background_blur} ${isScrolled && styles.scrolled}`}>
-        {windowDimensions.width > 790 &&
-          <div className={`${styles.navLink}`} >
-            <Link to="/">Home</Link>
-            <Link to="/gallery">Gallery</Link>
-            <Link to="/events">Events</Link>
-          </div>
-        }
-
-        {windowDimensions.width > 790 ?
-          <div className={styles["navbar-brand"]}>
-            <div className={styles["navbar-logo"]}>
-              <img src={require("../assets/Logo1.png")} alt="SKI logo" />
-
-            </div>
-            <div className={styles["titleText"]}>
-              <h1>SRI KRISHNA COLLEGE OF TECHNOLOGY</h1>
-              <h3>AUTONOMOUS INSTITUTION | ACCREDITED BY NAAC WITH ‘A’ GRADE</h3>
-            </div>
-
-            <div className={styles["navbar-logo"]}>
-              <img src={require("../assets/Logo2.png")} alt="SKCT logo" />
-            </div>
-
-          </div> :
-          <div className={styles["navbar-brand"]}>
-
-            {windowDimensions.width > 350 &&
-              <div className={styles["navbar-logo"]}>
-                <img src={require("../assets/Logo2.png")} alt="SKCT logo" />
-                <hr />
-
-              </div>
-            }
-            <div className={styles["titleText"]}>
-              <p>SKCT</p>
-            </div>
-
-          </div>
-        }
-        {windowDimensions.width > 790 ?
-          <div className={`${styles.navLink} ${styles.right}`}>
-            <Link to="/alumini-forum">Alumini Forum</Link>
-            <Link to="/office-bearers">Office Bearers</Link>
-          </div> :
-          <div className={`${styles.dropdown} }`}>
-            {!menuActive &&
-              <MenuIcon className={styles["dropdown-btn"]} onClick={() => { setMenuActive(true) }} />
-            }
-          </div>
-        }
-
-
-      </div >
-
-      {menuActive && windowDimensions.width < 790 &&
-        <Menu setMenuActive={setMenuActive} />
-      }
-
-    </div>
-
-  )
-}
->>>>>>> origin
 
 export default Navbar;
