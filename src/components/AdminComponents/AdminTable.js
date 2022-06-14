e5a32de419f1ed37707cb7c0afdd7a3fa9ad1679
Mohamed Aslam <mohamedaslammn.2003@gmail.com>
2022-06-14 12:27:17 +0530

    Admin Table Header

--- src/components/AdminComponents/AdminTable.js
+++ src/components/AdminComponents/AdminTable.js
@@ -1,9 +1,15 @@
 import Pagination from "components/UI/Pagination";
 import React, { useState } from "react";
+import AdminTableHeader from "./AdminTableHeader";
 import AdminTablePagination from "./AdminTablePagination";
 
 const AdminTable = () => {
-  return <AdminTablePagination />;
+  return (
+    <div>
+      <AdminTableHeader />
+      <AdminTablePagination />
+    </div>
+  );
 };
 
 export default AdminTable;
--- src/components/AdminComponents/AdminTableHeader.js
+++ src/components/AdminComponents/AdminTableHeader.js
@@ -1,31 +1,42 @@
+import Divider from "components/UI/Divider";
 import React from "react";
+import { FaFilter, FaPlus } from "react-icons/fa";
 import { FiSearch } from "react-icons/fi";
 import Styles from "./AdminTableHeader.module.css";
 
 const AdminTableHeader = () => {
   return (
-    <div>
-      <h1>Alumni Details</h1>
-      <div>
+    <>
+      <p className={Styles.table_header}>Alumni Details</p>
+      <Divider mb={5} mt={0} />
+      <div className={Styles.table_controls}>
         <div className={Styles.search}>
           <FiSearch />
           <input placeholder="Search Options" />
         </div>
-        <div>
-          <button>Filter</button>
-          <button>Insert</button>
+        <div className={Styles.table_actions}>
+          <button>
+            Filter <FaFilter />
+          </button>
+          <button>
+            Insert
+            <FaPlus />
+          </button>
         </div>
       </div>
-      <div>
-        <p>Showing 1-10 of 1000 results</p>
-        Results Per Page{" "}
-        <select>
-          <option>1</option>
-          <option>...</option>
-          <option>10</option>
-        </select>
+      <Divider mt={5} mb={-3} />
+      <div className={Styles.table_info_handler}>
+        <p className={Styles.no_of_results}>Showing 1-10 of 1000 results</p>
+        <div className={Styles.results_count_changer}>
+          <span>Results Per Page </span>
+          <select>
+            <option>1</option>
+            <option>...</option>
+            <option>10</option>
+          </select>
+        </div>
       </div>
-    </div>
+    </>
   );
 };
 
