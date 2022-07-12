3dc51f53a16e74dfe3f857be2bb2e923b0d8311b
Jayvan Andel S <75113307+jayvanandel07@users.noreply.github.com>
2022-07-12 17:04:44 +0530

    Revert "add social icons and revamp footer"

--- src/components/Layout/Footer.js
+++ src/components/Layout/Footer.js
@@ -1,6 +1,12 @@
 import React from "react";
 import styles from "./Footer.module.css";
-import { AiOutlineCopyrightCircle } from "react-icons/ai";
+import {
+  AiFillYoutube,
+  AiFillFacebook,
+  AiFillInstagram,
+  AiOutlineTwitter,
+  AiOutlineCopyrightCircle,
+} from "react-icons/ai";
 import { useLocation } from "react-router-dom";
 
 const Footer = () => {
@@ -13,25 +19,37 @@ const Footer = () => {
     isFooterVisible && (
       <div className={styles["footer"]}>
         <div className={styles["footer-container"]}>
-          <div className={styles["footer-brand"]}>
-            <img src={require('assets/Logo2.png')} alt="College-logo" />
-            <h1>SKCT</h1>
+          <div className={styles["top"]}>
+            <div className={styles["left"]}>
+              <div className={styles["name"]}>
+                <img src={require("assets/Logo2.png")} alt="SKCT logo" />
+                <p>Sri Krishna College of Technology Autonomus Institutuion</p>
+              </div>
+              <p>Mail Id: principal@skct.ac.in placement@skct.ac.in</p>
+            </div>
+            <div className={styles["right"]}>
+              <p>
+                Kovaipudur, Coimbatore TamilNadu - 641042 Phone: 0422-2678001,
+                Fax:0422-2678012
+              </p>
+            </div>
           </div>
-          <div className={styles["footer-description"]}>
-            <p>Copyright </p>
-            <AiOutlineCopyrightCircle />
-            <p>2022 SKCT - All Rights Reserved | Developed by InExore</p>
+          <hr />
+          <div className={styles["bottom"]}>
+            <div className={styles["left"]}>
+              <AiOutlineCopyrightCircle fontSize="small" />
+              <p>2022 SKCT - All Rights Reserved | Designed by InExore</p>
+            </div>
+            <div className={styles["right"]}>
+              <p>Follow us on</p>
+              <AiFillYoutube fontSize="medium" />
+              <AiFillFacebook fontSize="medium" />
+              <AiFillInstagram fontSize="medium" />
+              <AiOutlineTwitter fontSize="medium" />
+            </div>
           </div>
-          <div className={styles["social"]}>
-            <img src={require("assets/icons/map.png")} alt="map icon" />
-            <img src={require("assets/icons/YouTube.png")} alt="Youtube icon" />
-            <img src={require("assets/icons/Facebook.png")} alt="Facebook icon" />
-            <img src={require("assets/icons/Instagram.png")} alt="Instagram icon" />
-            <img src={require("assets/icons/Twitter.png")} alt="Twitter icon" />
-          </div>
-
         </div>
-      </div >
+      </div>
     )
   );
 };
