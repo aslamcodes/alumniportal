c0f3b4c9db6816f74c94799600618d8e32c539df
jayvanandel07 <j.vanandel07@gmail.com>
2022-07-14 00:28:10 +0530

    did the logic and description topbar

--- src/components/ForumComponents/ProfileModal.js
+++ src/components/ForumComponents/ProfileModal.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState } from 'react'
 import ReactPortal from "components/Modal/ReactPortal";
 import ForumCard from "components/ForumComponents/ForumCard";
 import styles from "./ProfileModal.module.css";
@@ -130,10 +130,29 @@ const PROFILE_IMAGES = [
 
 
 function ProfileModal() {
+  const [show, setShow] = useState({
+    desc: true,
+    post: false
+  });
   const pick_image = () => {
     const random_number = Math.floor(Math.random() * PROFILE_IMAGES.length);
     return PROFILE_IMAGES[random_number];
   }
+
+  const handleShow = (name) => {
+    if (name === "desc") {
+      setShow({
+        desc: true,
+        post: false
+      });
+    } else {
+      setShow({
+        desc: false,
+        post: true
+      });
+    }
+  }
+
   return (
     <ReactPortal>
       <div className={styles.profile_overlay} >
@@ -146,11 +165,13 @@ function ProfileModal() {
               <img src={require("assets/test/profile_photo.png")} alt="profile_img" />
             </div>
             <div className={styles.profile_info}>
-              <h2>jenifer aswin</h2>
+              <h2>Jenifer Aswin</h2>
               <h3>Software developer</h3>
               <p className={styles.location}>coimbatore,india</p>
-              <h3>Available on</h3>
+              <h4>Available on</h4>
               <div className={styles.social}>
+                <p>slack</p>
+                <p>slack</p>
                 <p>slack</p>
                 <p>slack</p>
                 <p>slack</p>
@@ -158,22 +179,35 @@ function ProfileModal() {
             </div>
             <div className={styles.profile_description_container}>
               <div className={styles.description_topbar}>
-                <p>Self Description</p>
-                <p>Post</p>
+                <p className={`${show.desc && styles.selected}`} onClick={() => handleShow("desc")}>Self Description</p>
+                <p className={`${show.post && styles.selected}`} onClick={() => handleShow("post")}>Post</p>
               </div>
-              <div className={styles.description}>
-                <p>
-                  Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-                  Pellentesque euismod, nisi vel consectetur euismod,
-                  nisi nisl aliquet nisl, eget euismod nisl nisl eget
-                  nisi.
-                </p>
-              </div>
-              <div className={styles.posts}>
-                {DUMMY_POST_DATA.map((post) => {
-                  return <ForumCard key={post.id} data={post} />
-                })}
-              </div>
+              {show.desc && (
+                <div className={styles.description}>
+                  <p>
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                    Pellentesque euismod, nisi vel consectetur euismod,
+                    nisi nisl aliquet nisl, eget euismod nisl nisl eget
+                    nisi.Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                    Pellentesque euismod, nisi vel consectetur euismod,
+                    nisi nisl aliquet nisl, eget euismod nisl nisl eget
+                    nisi.Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                    Pellentesque euismod, nisi vel consectetur euismod,
+                    nisi nisl aliquet nisl, eget euismod nisl nisl eget
+                    nisi.Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                    Pellentesque euismod, nisi vel consectetur euismod,
+                    nisi nisl aliquet nisl, eget euismod nisl nisl eget
+                    nisi.
+                  </p>
+                </div>
+              )}
+              {show.post && (
+                <div className={styles.posts}>
+                  {DUMMY_POST_DATA.map((post) => {
+                    return <ForumCard key={post.id} data={post} />
+                  })}
+                </div>
+              )}
             </div>
           </div>
         </div>
