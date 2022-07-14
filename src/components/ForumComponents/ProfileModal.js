12bb96ce4ba5d17991ab449dcde297b46631a86b
jayvanandel07 <j.vanandel07@gmail.com>
2022-07-14 17:16:50 +0530

    make profile editable

--- src/components/ForumComponents/ProfileModal.js
+++ src/components/ForumComponents/ProfileModal.js
@@ -135,9 +135,44 @@ function ProfileModal({ handleClose }) {
     post: false
   });
   const [editProfile, setEditProfile] = useState(false);
+
+  const [profileData, setProfileData] = useState({
+    profile_image: "",
+    name: "Jenifer Aswin",
+    designation: "Software Developer",
+    location: "Coimbatore, India",
+    social: {
+      slack: "https://join.slack.com/team_test/shared_inviteas",
+      twitter: "https://twitter.com/team_test/shared_inviteas",
+      gmail: "teamwork123_a@gmail.com",
+      gitHub: "https://github.com/team_test/shared_inviteas",
+      facebook: "Enter the Facebook link",
+      linkedin: "Enter Linkedin Account Link",
+    },
+    description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.Pellentesque euismod, nisi vel consectetur euismod,nisi nisl aliquet nisl, eget euismod nisl nisl eget nisi.Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque euismod, nisi vel consectetur euismod, nisi nisl aliquet nisl, eget euismod nisl nisl egenisi.Lorem ipsum dolor sit amet, consectetur adipiscing elit.Pellentesque euismod, nisi vel consectetur euismod,nisi nisl aliquet nisl, eget euismod nisl nisl egetnisi.Lorem ipsum dolor sit amet, consectetur adipiscing elit.Pellentesque euismod, nisi vel consectetur euismod,nisi nisl aliquet nisl, eget euismod nisl nisl egetnisi.",
+
+    other: {
+      company: "Company Name",
+      organization: "Organisation Name",
+      skills: "Skills / Domain",
+      contact: "Contact Number",
+      ugCollege: "Under Graduation College Name",
+      ugCourse: "Under  Graduation Course Name",
+      pgCollege: "Post Graduation College Name if any",
+      pgCourse: "Post Graduation Course Name if any"
+    }
+  })
+
   const pick_image = () => {
     const random_number = Math.floor(Math.random() * PROFILE_IMAGES.length);
     return PROFILE_IMAGES[random_number];
+  }
+
+  const handleChange = (e, name) => {
+    setProfileData({
+      ...profileData,
+      [name]: e.currentTarget.textContent
+    })
   }
 
   const handleShow = (name) => {
@@ -173,27 +208,53 @@ function ProfileModal({ handleClose }) {
             <div className={styles.profile_img}>
               <img src={require("assets/test/profile_photo.png")} alt="profile_img" />
               {editProfile &&
-                <label for="img-switch" >
+                <label htmlFor="img-switch" >
                   <img src={require("assets/image-switch.png")} alt="image-switch-icon" />
                 </label>}
               <input name="image" id="img-switch" type="file" />
 
             </div>
-            <div className={styles.profile_info}>
-              <h2>Jenifer Aswin</h2>
-              <h3>Software developer</h3>
-              <div className={styles.location}>
+            <div className={`${styles.profile_info} ${editProfile && styles.profile_info_edit}`}>
+              <h2 className={`${editProfile && styles.editActive}`} contentEditable={editProfile} suppressContentEditableWarning={true} onBlur={(e) => handleChange(e, "name")}>{profileData.name}</h2>
+              <h3 className={`${editProfile && styles.editActive}`} contentEditable={editProfile} suppressContentEditableWarning={true} onBlur={(e) => handleChange(e, "designation")}>{profileData.designation}</h3>
+              <div className={`${styles.location} ${editProfile && styles.editActive}`}>
                 <img src={require("assets/icons/location.png")} alt="location icon" />
-                <p>Coimbatore,India</p>
+                <p suppressContentEditableWarning={true} contentEditable={editProfile} onBlur={(e) => handleChange(e, "location")}>{profileData.location}</p>
               </div>
               <h4>Available on</h4>
-              <div className={styles.social}>
+              {!editProfile ? <div className={styles.social}>
                 <img src={require("assets/icons/social/slack.png")} alt="slack icon" />
                 <img src={require("assets/icons/social/twitter.png")} alt="twitter icon" />
                 <img src={require("assets/icons/social/gmail.png")} alt="gmail icon" />
                 <img src={require("assets/icons/social/gitHub.png")} alt="gitHub icon" />
                 <img src={require("assets/icons/social/facebook.png")} alt="facebook icon" />
               </div>
+                :
+                <div className={styles.editSocial_container}>
+                  {Object.keys(profileData.social).map((key, index) => {
+                    return (
+                      <div key={index} className={`${styles.editSocial} ${editProfile && styles.editActive}`}>
+                        <p suppressContentEditableWarning={true} contentEditable={editProfile} onBlur={(e) => handleChange(e, `social.${key}`)} >{profileData.social[key]}</p>
+                        <img src={require(`assets/icons/social/${key}.png`)} alt="social icon" />
+                      </div>
+                    )
+                  })}
+                </div>
+              }
+              {editProfile &&
+                <div className={styles.otherDetails}>
+                  {Object.keys(profileData.other).map((key, index) => {
+                    return (
+                      <p key={index} className={styles.editActive} suppressContentEditableWarning={true} contentEditable={editProfile} onBlur={(e) => handleChange(e, `other.${key}`)} >{profileData.other[key]}</p>
+                    )
+                  }
+                  )}
+                </div>
+              }
+
+
+
+
               {!editProfile ?
                 <div className={styles.edit_profile} onClick={() => setEditProfile(true)}>
                   <p>Edit Profile</p>
@@ -213,21 +274,7 @@ function ProfileModal({ handleClose }) {
               </div>
               {show.desc && (
                 <div className={styles.description}>
-                  <p>
-                    Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-                    Pellentesque euismod, nisi vel consectetur euismod,
-                    nisi nisl aliquet nisl, eget euismod nisl nisl eget
-                    nisi.Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-                    Pellentesque euismod, nisi vel consectetur euismod,
-                    nisi nisl aliquet nisl, eget euismod nisl nisl eget
-                    nisi.Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-                    Pellentesque euismod, nisi vel consectetur euismod,
-                    nisi nisl aliquet nisl, eget euismod nisl nisl eget
-                    nisi.Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-                    Pellentesque euismod, nisi vel consectetur euismod,
-                    nisi nisl aliquet nisl, eget euismod nisl nisl eget
-                    nisi.
-                  </p>
+                  <p className={`${editProfile && styles.editActive}`} suppressContentEditableWarning={true} contentEditable={editProfile} onBlur={(e) => handleChange(e, "description")}>{profileData.description}</p>
                 </div>
               )}
               {show.post && (
