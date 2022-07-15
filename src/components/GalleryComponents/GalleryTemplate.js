f7ec91c444a7bdd7a9953be64de4d9b4afb1b675
jayvanandel07 <j.vanandel07@gmail.com>
2022-07-16 00:04:41 +0530

    global tap highlighter to transparent and gallery logic and transition

--- src/components/GalleryComponents/GalleryTemplate.js
+++ src/components/GalleryComponents/GalleryTemplate.js
@@ -1,21 +1,44 @@
-import React from 'react'
+import React, { useState } from 'react'
 import styles from "./GalleryTemplate.module.css"
 
+const imgSrc = "https://source.unsplash.com/random/"
+
+// test data:
+const dimension = ["300x300", "400x400", "500x500", "600x600", "700x700", "800x800", "900x900", "1000x1000"];
+let images = [];
+for (let i = 0; i < 10; i++) {
+  let d = Math.floor(Math.random() * 8);
+  let dm = dimension[d];
+  let Src = imgSrc.concat(dm);
+  images.push(Src);
+}
+
 function GalleryTemplate({ fname, sname, data }) {
-  const imgSrc = "https://source.unsplash.com/random/"
+  const [imageSwitch, setImageSwitch] = useState(
+    {
+      imageActive: 0,
+      imageTop: null,
+      imageBottom: 1,
+    }
+  )
 
-  // test data:
-  const dimension = ["300x300", "400x400", "500x500", "600x600", "700x700", "800x800", "900x900", "1000x1000"];
-  let images = [];
-  for (let i = 0; i < 10; i++) {
-    let d = Math.floor(Math.random() * 8);
-    let dm = dimension[d];
-    let Src = imgSrc.concat(dm);
-    console.log('src: ', Src);
-    const style = { "--delay": i + 1 }
-    images.push(<img key={i} src={Src} delay={i + 1} alt="" />);
+
+  const handleScroll = event => {
+    console.log('scrollTop: ', event.currentTarget.scrollTop);
+    console.log('offsetHeight: ', event.currentTarget.scrollBottom);
+  };
+
+  const handleClick = (e) => {
+
+    let id = parseInt(e.target.id);
+    setImageSwitch({
+      imageActive: id,
+      imageTop: id - 1,
+      imageBottom: id + 1,
+    })
+    console.log('id: ', id);
+    console.log('imageSwitch: ', imageSwitch);
   }
-
 
   return (
     <div className={styles.gallery_container}>
@@ -23,11 +46,16 @@ function GalleryTemplate({ fname, sname, data }) {
         <h1>{fname} <span>{sname}</span></h1>
       </div>
 
-      <div className={styles.gallery_content}>
-        <img key={1} src={"https://source.unsplash.com/random/400x400"} delay={1} alt="" />
-        <img key={2} src={"https://source.unsplash.com/random/500x500"} delay={2} alt="" />
-        <img key={3} src={"https://source.unsplash.com/random/600x600"} delay={3} alt="" />
-        <img key={3} src={"https://source.unsplash.com/random/700x700"} delay={3} alt="" />
+      <div className={styles.gallery_content} onScroll={handleScroll}>
+        {images.map((image, index) => {
+          return (
+            <img key={index} id={index} src={image} alt="" className={
+              `${imageSwitch.imageActive === index && styles.image_active || imageSwitch.imageTop === index && styles.image_top || imageSwitch.imageBottom === index && styles.image_bottom}`
+            }
+              onClick={handleClick}
+            />
+          )
+        })}
 
       </div>
     </div>
