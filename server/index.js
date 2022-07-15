0767d73d8416c765b7859060be277290d26fdd70
Mohamed Aslam <mohamedaslammn.2003@gmail.com>
2022-07-16 00:52:51 +0530

    feat(server): :sparkles: Introduce base routers and Controllers for Gallery

--- server/index.js
+++ server/index.js
@@ -5,6 +5,7 @@ import userRouter from "./routes/userRoutes.js";
 import eventRouter from "./routes/eventRoutes.js";
 import alumniRouter from "./routes/alumniRoutes.js";
 import forumRouter from "./routes/forumRoutes.js";
+import galleryRouter from "./routes/galleryRoutes.js";
 import { notFound, errorHandler } from "./middleware/errorMiddleware.js";
 import path from "path";
 import { fileURLToPath } from "url";
@@ -22,6 +23,7 @@ app.use("/api/v1/users", userRouter);
 app.use("/api/v1/alumni", alumniRouter);
 app.use("/api/v1/events", eventRouter);
 app.use("/api/v1/forum", forumRouter);
+app.use("/api/v1/gallery", galleryRouter);
 
 if (process.env.NODE_ENV === "DEVELOPMENT") {
   console.log("Development mode".yellow);
