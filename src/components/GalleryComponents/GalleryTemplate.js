90f37a52cc1f3d80aa1b710387ca463ecede0d20
jayvanandel07 <j.vanandel07@gmail.com>
2022-07-16 14:21:07 +0530

    make gallery template responsive

--- src/components/GalleryComponents/GalleryTemplate.js
+++ src/components/GalleryComponents/GalleryTemplate.js
@@ -1,19 +1,22 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import styles from "./GalleryTemplate.module.css"
 
-const imgSrc = "https://source.unsplash.com/random/"
 
-// test data:
-const dimension = ["300x300", "400x400", "500x500", "600x600", "700x700", "800x800", "900x900", "1000x1000"];
-let images = [];
-for (let i = 0; i < 10; i++) {
-  let d = Math.floor(Math.random() * 8);
-  let dm = dimension[d];
-  let Src = imgSrc.concat(dm);
-  images.push(Src);
+function getWindowDimensions() {
+  const { innerWidth: width, innerHeight: height } = window;
+  return {
+    width,
+    height,
+  };
 }
 
+
 function GalleryTemplate({ fname, sname, data }) {
+  const [windowDimensions, setWindowDimensions] = useState(
+    getWindowDimensions()
+  );
+
+  const [noImage, setNoImage] = useState(1);
   const [imageSwitch, setImageSwitch] = useState(
     {
       imageActive: 0,
@@ -22,6 +25,43 @@ function GalleryTemplate({ fname, sname, data }) {
     }
   )
 
+  let Data = [];
+  const testData = (n) => {
+    // test data:
+    const imgSrc = "https://source.unsplash.com/random/"
+    const dimension = ["300x300", "400x400", "500x500", "600x600", "700x700", "800x800", "900x900", "1000x1000"];
+    for (let i = 0; i < n; i++) {
+      let images = [];
+      for (let j = 0; j < 10; j++) {
+        let d = Math.floor(Math.random() * 8);
+        let dm = dimension[d];
+        let Src = imgSrc.concat(dm);
+        images.push(Src);
+      }
+      Data.push(images);
+    }
+    return Data;
+  }
+
+  Data = testData(noImage);
+
+
+  console.log('Data: ', Data);
+  useEffect(() => {
+    function handleResize() {
+      setWindowDimensions(getWindowDimensions());
+    }
+    console.log('noImage: ', noImage);
+    if (windowDimensions.width > 1020) {
+      setNoImage(3);
+    } else if (windowDimensions.width > 780) {
+      setNoImage(2);
+    } else {
+      setNoImage(1);
+    }
+    window.addEventListener("resize", handleResize);
+    return () => window.removeEventListener("resize", handleResize);
+  }, []);
 
   const handleScroll = event => {
     console.log('scrollTop: ', event.currentTarget.scrollTop);
@@ -47,15 +87,24 @@ function GalleryTemplate({ fname, sname, data }) {
       </div>
 
       <div className={styles.gallery_content} onScroll={handleScroll}>
-        {images.map((image, index) => {
+
+        {console.log('Data render', Data)}
+        {Data.map((images, index1) => {
           return (
-            <img key={index} id={index} src={image} alt="" className={
-              `${imageSwitch.imageActive === index && styles.image_active || imageSwitch.imageTop === index && styles.image_top || imageSwitch.imageBottom === index && styles.image_bottom}`
-            }
-              onClick={handleClick}
-            />
+            <div key={index1} className={styles.gallery_img}>
+              {images.map((image, index) => {
+                return (
+                  <img key={index} id={index} src={image} alt="" className={
+                    `${imageSwitch.imageActive === index && styles.image_active || imageSwitch.imageTop === index && styles.image_top || imageSwitch.imageBottom === index && styles.image_bottom}`
+                  }
+                    onClick={handleClick}
+                  />
+                )
+              })}
+            </div>
           )
         })}
+
 
       </div>
     </div>
