059a2280471f048ddec62dd7eeba79453e6ce758
Mohamed Aslam <mohamedaslammn.2003@gmail.com>
2022-07-21 11:20:05 +0530

    perf(server): :zap: Delet Replies related post when it got deleted

--- server/models/ForumComment.js
+++ server/models/ForumComment.js
@@ -1,4 +1,5 @@
 import mongoose from "mongoose";
+import ForumCommentReply from "./ForumCommentReply.js";
 
 const commentSchema = new mongoose.Schema(
   {
@@ -21,6 +22,21 @@ const commentSchema = new mongoose.Schema(
     timestamps: true,
   }
 );
+
+// commentSchema.pre("deleteMany", async function (next) {
+//   try {
+//     const post = this.getQuery()._id;
+//     // comments to delete
+//     const commentsToBeDeleted = await ForumCommentReply.find({
+//       post: post,
+//     });
+//     // delete replies
+
+//     await ForumCommentReply.deleteMany({ comment: post });
+//   } catch (error) {
+//     next(error);
+//   }
+// });
 
 const Comment = mongoose.model("ForumComment", commentSchema);
 
--- server/models/ForumPost.js
+++ server/models/ForumPost.js
@@ -1,6 +1,7 @@
 import mongoose from "mongoose";
 import Comment from "../models/ForumComment.js";
 import Like from "../models/ForumPostLike.js";
+import ForumCommentReply from "./ForumCommentReply.js";
 const { Schema, model } = mongoose;
 
 const postSchema = new Schema(
@@ -35,6 +36,14 @@ const postSchema = new Schema(
 postSchema.pre("deleteOne", async function (next) {
   const postId = this.getQuery()._id;
   try {
+    const comments = await Comment.find({
+      post: postId,
+    });
+    await ForumCommentReply.deleteMany({
+      comment: {
+        $in: comments.map((comment) => comment._id),
+      },
+    });
     await Comment.deleteMany({ post: postId });
     await Like.deleteOne({ post: postId });
   } catch (error) {
