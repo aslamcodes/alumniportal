564cf80bdd4f60d5388b72fdc2a546a3b85b69e0
Mohamed Aslam <mohamedaslammn.2003@gmail.com>
2022-07-24 12:15:07 +0530

    feat(server): :sparkles: Create base notification schema

---
+++ server/models/Notification.js
@@ -0,0 +1,58 @@
+import mongoose from "mongoose";
+
+const { model, Schema } = mongoose;
+
+const notificationSchema = new Schema({
+  user: {
+    type: Schema.Types.ObjectId,
+    ref: "User",
+    required: true,
+  },
+  message: {
+    type: String,
+    required: true,
+  },
+  type: {
+    type: String,
+    required: true,
+    enum: [
+      "like",
+      "comment",
+      "reply",
+      "alumni-request",
+      "alumni-reject",
+      "alumni-approved",
+      "post-created",
+      "post-updated",
+      "post-deleted",
+    ],
+  },
+  resolved: {
+    type: Boolean,
+    default: false,
+  },
+  post: {
+    type: Schema.Types.ObjectId,
+    ref: "ForumPost",
+    required: false,
+  },
+  like: {
+    type: Schema.Types.ObjectId,
+    ref: "ForumPostLike",
+    required: false,
+  },
+  comment: {
+    type: Schema.Types.ObjectId,
+    ref: "ForumComment",
+    required: false,
+  },
+  alumniRequest: {
+    type: Schema.Types.ObjectId,
+    ref: "AlumniRequest",
+    required: false,
+  },
+});
+
+const Notification = model(notificationSchema, "Notification");
+
+export default Notification;
