840cb3df4789ec8cb223e75fac009f455906113a
Mohamed Aslam <mohamedaslammn.2003@gmail.com>
2022-07-27 23:09:37 +0530

    feat(server): :fire: Remove Alumni from create account options

--- client/src/components/RegistrationComponents/RegisterOptions.js
+++ client/src/components/RegistrationComponents/RegisterOptions.js
@@ -1,18 +1,20 @@
-import React from 'react'
-import { useNavigate } from 'react-router-dom';
+import React from "react";
+import { useNavigate } from "react-router-dom";
 import styles from "./RegisterOptions.module.css";
 function RegisterOptions({ active }) {
   const navigate = useNavigate();
   return (
-    <div className={`${styles.register_options_container} ${active && styles.register_options_container_active}`}>
+    <div
+      className={`${styles.register_options_container} ${
+        active && styles.register_options_container_active
+      }`}
+    >
       <ul>
-
-        <li onClick={() => navigate("/register-alumni")}>Alumni</li>
         <li onClick={() => navigate("/register-student")}>Student</li>
         <li onClick={() => navigate("/register-faculty")}>Faculty</li>
       </ul>
     </div>
-  )
+  );
 }
 
-export default RegisterOptions
+export default RegisterOptions;
