c3fdc0bd3964a6907e906993020f2eeaa9b55dc7
Mohamed Aslam <mohamedaslammn.2003@gmail.com>
2022-07-31 13:27:38 +0530

    feat(server-client): :sparkles: Fetch Events from the server

--- controllers/eventControllers.js
+++ controllers/eventControllers.js
@@ -2,17 +2,19 @@ import Event from "../models/Event.js";
 import asyncHandler from "express-async-handler";
 
 export const createEvent = asyncHandler(async (req, res) => {
-  const { eventName, date, venue, time } = req.body;
+  const { eventName, startDate, endDate, venue } = req.body;
   const event = await Event.create({
     eventName,
-    date,
+    startDate,
+    endDate,
     venue,
   });
   if (event) {
     res.status(201).json({
       _id: event._id,
       eventName: event.eventName,
-      date: event.date,
+      startDate: event.startDate,
+      endDate: event.endDate,
       venue: event.venue,
       time: event.time,
     });
@@ -46,14 +48,9 @@ export const getEventById = asyncHandler(async (req, res) => {
 });
 
 export const updateEvent = asyncHandler(async (req, res) => {
-  const { eventName, date, venue } = req.body;
-  const event = await Event.findByIdAndUpdate(
-    req.params.id,
-    { eventName, date, venue },
-    {
-      new: true,
-    }
-  );
+  const event = await Event.findByIdAndUpdate(req.params.id, req.body, {
+    new: true,
+  });
   if (event) {
     res.status(200).json(event);
   } else {
