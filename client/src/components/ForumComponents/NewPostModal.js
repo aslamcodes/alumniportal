f124f5aa9af3557297a350c73ae81d775b8f987c
Mohamed Aslam <mohamedaslammn.2003@gmail.com>
2022-08-05 16:21:13 +0530

    Remove Console logs

--- client/src/components/ForumComponents/NewPostModal.js
+++ client/src/components/ForumComponents/NewPostModal.js
@@ -6,6 +6,7 @@ import styles from "./NewPostModal.module.css";
 import { GiPin } from "react-icons/gi";
 import { useAuthContext } from "context/auth/authContext";
 import useAxiosWithCallback from "hooks/useAxiosWithCallback";
+import Loader from "components/UI/Loader";
 
 function NewPostModal({ setNewPostActive }) {
   const [imageSwitch, setImageSwitch] = useState(false);
@@ -43,6 +44,10 @@ function NewPostModal({ setNewPostActive }) {
     };
   }, [setNewPostActive]);
 
+  useEffect(() => {
+    if (error) alert(error?.response?.data?.message);
+  }, [error]);
+
   const handleMouseEnter = () => {
     setImageSwitch(true);
   };
@@ -60,133 +65,143 @@ function NewPostModal({ setNewPostActive }) {
     e.preventDefault();
     const postFormData = new FormData();
 
-    postFormData.append("post", {
-      title: postData.title,
-      desc: postData.description,
-    });
-    postFormData.append("post_images", [postData.image]);
+    postFormData.append("title", postData.title);
+    postFormData.append("desc", postData.description);
+    postFormData.append("post_images", [].push(postData.image));
 
     const postConfig = {
       url: "/api/v1/forum",
       method: "post",
+      headers: {
+        Authorization: `Bearer ${user?.token}`,
+        "content-type": "multipart/form-data",
+      },
       data: postFormData,
     };
 
-    await createPost(postConfig, (res) => {});
+    await createPost(postConfig, (res) => {
+      alert("Post Made Successfully");
+    });
   };
 
   return (
     <ReactPortal wrapperId="new_post_modal_wrapper">
       <div className={styles.new_post_overlay}>
         <div className={styles.new_post_container}>
-          <div className={styles.header}>
-            <div className={styles.userinfo_container}>
-              <img
-                alt="profile"
-                src={`http://localhost:8000/api/v1/users/user-avatar/${user?._id}`}
-              />
-              <p className={styles.user_name}>{user?.name}</p>
-            </div>
-            <IoClose
-              size="30px"
-              className={styles.close_icon}
-              onClick={() => setNewPostActive(false)}
-            />
-          </div>
-          <div className={styles.body}>
-            <form onSubmit={handleSubmit}>
-              <div className={styles.input_container}>
-                <input
-                  name="cname"
-                  type="text"
-                  id="cname"
-                  placeholder="Company Name"
-                  value={postData.cname}
-                  onChange={handleChange}
-                />
-              </div>
-              <div className={styles.input_container}>
-                <input
-                  type="text"
-                  name="designation"
-                  id="designation"
-                  placeholder="Designation"
-                  value={postData.designation}
-                  onChange={handleChange}
-                />
-              </div>
-              <div className={styles.description_container}>
-                <div className={styles.input_container}>
-                  <input
-                    type="text"
-                    name="title"
-                    id="title"
-                    placeholder="title"
-                    value={postData.title}
-                    onChange={handleChange}
-                  />
-                </div>
-                <div className={styles.input_container}>
-                  <textarea
-                    type="text"
-                    name="description"
-                    id="description"
-                    placeholder="description"
-                    value={postData.description}
-                    onChange={handleChange}
-                  />
-                </div>
-              </div>
-
-              <div className={styles.img_input_container}>
-                {postData.image && imageSwitch && (
-                  <label for="img-switch">
-                    <img
-                      src={require("assets/image-switch.png")}
-                      alt="switch-icon"
-                      onMouseEnter={handleMouseEnter}
-                      onMouseLeave={handleMouseLeave}
-                    />
-                  </label>
-                )}
-                <input
-                  name="image"
-                  id="img-switch"
-                  type="file"
-                  onChange={onSelectFile}
-                />
-
-                {postData.image ? (
+          {isLoading ? (
+            <Loader />
+          ) : (
+            <>
+              <div className={styles.header}>
+                <div className={styles.userinfo_container}>
                   <img
-                    className={`${imageSwitch && styles.image_blur}`}
-                    src={URL.createObjectURL(postData.image)}
-                    alt="upload"
-                    onMouseEnter={handleMouseEnter}
-                    onMouseLeave={handleMouseLeave}
+                    alt="profile"
+                    src={`http://localhost:8000/api/v1/users/user-avatar/${user?._id}`}
                   />
-                ) : (
-                  <div className={styles.img_input}>
-                    <label for="img-input">
-                      <BiImageAdd size="50px" />
-                      <p>Add Image</p>
-                    </label>
+                  <p className={styles.user_name}>{user?.name}</p>
+                </div>
+                <IoClose
+                  size="30px"
+                  className={styles.close_icon}
+                  onClick={() => setNewPostActive(false)}
+                />
+              </div>
+              <div className={styles.body}>
+                <form onSubmit={handleSubmit}>
+                  <div className={styles.input_container}>
+                    <input
+                      name="cname"
+                      type="text"
+                      id="cname"
+                      placeholder="Company Name"
+                      value={postData.cname}
+                      onChange={handleChange}
+                    />
+                  </div>
+                  <div className={styles.input_container}>
+                    <input
+                      type="text"
+                      name="designation"
+                      id="designation"
+                      placeholder="Designation"
+                      value={postData.designation}
+                      onChange={handleChange}
+                    />
+                  </div>
+                  <div className={styles.description_container}>
+                    <div className={styles.input_container}>
+                      <input
+                        type="text"
+                        name="title"
+                        id="title"
+                        placeholder="title"
+                        value={postData.title}
+                        onChange={handleChange}
+                      />
+                    </div>
+                    <div className={styles.input_container}>
+                      <textarea
+                        type="text"
+                        name="description"
+                        id="description"
+                        placeholder="description"
+                        value={postData.description}
+                        onChange={handleChange}
+                      />
+                    </div>
+                  </div>
+
+                  <div className={styles.img_input_container}>
+                    {postData.image && imageSwitch && (
+                      <label for="img-switch">
+                        <img
+                          src={require("assets/image-switch.png")}
+                          alt="switch-icon"
+                          onMouseEnter={handleMouseEnter}
+                          onMouseLeave={handleMouseLeave}
+                        />
+                      </label>
+                    )}
                     <input
                       name="image"
-                      id="img-input"
+                      id="img-switch"
                       type="file"
                       onChange={onSelectFile}
                     />
+
+                    {postData.image ? (
+                      <img
+                        className={`${imageSwitch && styles.image_blur}`}
+                        src={URL.createObjectURL(postData.image)}
+                        alt="upload"
+                        onMouseEnter={handleMouseEnter}
+                        onMouseLeave={handleMouseLeave}
+                      />
+                    ) : (
+                      <div className={styles.img_input}>
+                        <label for="img-input">
+                          <BiImageAdd size="50px" />
+                          <p>Add Image</p>
+                        </label>
+                        <input
+                          name="image"
+                          id="img-input"
+                          type="file"
+                          onChange={onSelectFile}
+                        />
+                      </div>
+                    )}
                   </div>
-                )}
+                  <button className={styles.post_btn} type="submit">
+                    <div className={styles.post_name}>Post</div>
+                    <div className={styles.post_icon}>
+                      <GiPin />
+                    </div>
+                  </button>
+                </form>
               </div>
-              <button className={styles.post_btn} type="submit">
-                <div className={styles.post_name}>Post</div>
-                <div className={styles.post_icon}>
-                  <GiPin />
-                </div>
-              </button>
-            </form>
-          </div>
+            </>
+          )}
         </div>
       </div>
     </ReactPortal>
