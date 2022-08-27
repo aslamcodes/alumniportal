1629d7609e4ef30ebeda859364f68b47add8a8ff
jayvanandel07 <j.vanandel07@gmail.com>
2022-08-28 01:19:11 +0530

    add chat card

--- client/src/components/MessageComponents/ChatCard.js
+++ client/src/components/MessageComponents/ChatCard.js
@@ -1,8 +1,28 @@
 import React from 'react'
-
+import { IoIosArrowForward } from 'react-icons/io'
+import styles from "./ChatCard.module.css"
 const ChatCard = () => {
   return (
-    <div>ChatCard</div>
+    <div className={styles.chatCard_container}>
+      <div className={styles.chatCard_profile}>
+        <img src={require("assets/christopher.jpg")} alt="" />
+      </div>
+      <div className={styles.chatCard_body}>
+        <div className={styles.chatCard_details}>
+          <div>
+            <h3>
+              Jennifer
+            </h3>
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit.
+              {/* Lorem ipsum dolor sit, amet consectetur adipisicing elit. Corrupti non velit mollitia, nostrum earum dolorem ut et, sequi ullam quisquam voluptatibus exercitationem blanditiis ipsam corporis quis, doloribus accusantium alias nemo! */}
+            </p>
+          </div>
+          <IoIosArrowForward className={styles.arrow_forward_btn} fontSize={20} />
+        </div>
+        <hr />
+      </div>
+    </div>
   )
 }
 
