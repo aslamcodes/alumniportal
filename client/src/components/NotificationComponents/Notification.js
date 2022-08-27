0bf81c9eecb96b01347df2aa7daecaa2a016e843
jayvanandel07 <j.vanandel07@gmail.com>
2022-08-27 22:38:56 +0530

    add functionality

--- client/src/components/NotificationComponents/Notification.js
+++ client/src/components/NotificationComponents/Notification.js
@@ -1,17 +1,26 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { IoClose } from 'react-icons/io5'
 import { RiArrowDropDownLine } from 'react-icons/ri'
 import styles from "./Notification.module.css"
 const Notification = () => {
+  const [isHovered, setIsHovered] = useState(false);
+  const [isExpanded, setIsExpanded] = useState(false);
+  const handleDelete = () => {
+    console.log('clicked');
+  }
 
   return (
     <div className={styles.notification_container}>
-      <div className={`${styles.notification_card} ${styles.expanded}`}>
+      {/* Map notification_card */}
+      <div className={`${styles.notification_card} ${isExpanded && styles.expanded}`}
+        onMouseEnter={() => setIsHovered(true)}
+        onMouseLeave={() => setIsHovered(false)}
+      >
         <div className={styles.message}>
           <p>Your approval has been accepted Lorem ipsum dolor, sit amet consectetur adipisicing elit. Harum, saepe? Officiis tempore esse ab sint a voluptate quaerat dignissimos doloremque odit, tempora blanditiis eaque vitae atque rerum laboriosam doloribus quo.</p>
         </div>
-        <RiArrowDropDownLine fontSize={30} className={styles.arrow_btn} />
-        <IoClose className={styles.close_btn} />
+        {isHovered && <RiArrowDropDownLine fontSize={30} className={styles.arrow_btn} onClick={() => setIsExpanded(!isExpanded)} />}
+        {isHovered && <IoClose className={styles.close_btn} onClick={handleDelete} />}
       </div>
     </div>
   )
