73ce79988298e20431a6f426c6fc101c7be81479
jayvanandel07 <j.vanandel07@gmail.com>
2022-08-27 13:51:51 +0530

    add open close function

--- client/src/components/GalleryComponents/AddPhotos.js
+++ client/src/components/GalleryComponents/AddPhotos.js
@@ -14,44 +14,51 @@ const AddPhotos = ({ isCardActive }) => {
     setImage(
       e.target.files[0],
     );
+
   };
 
+  const handleSubmit = () => {
+    console.log('');
+  }
   return (
-
-    <div className={styles.add_photos_container} onClick={(e) => e.stopPropagation()}>
-      <div className={styles.image_input_container}
-        onMouseEnter={() => setImageSwitch(true)}
-        onMouseLeave={() => setImageSwitch(false)}
-      >
-        <label for="img-input" alt="switch-icon">
-          {image && imageSwitch && (
-            <img
-              src={require("assets/image-switch.png")}
-              alt="switch-icon"
+    <>
+      {isCardActive && (
+        <div className={styles.add_photos_container} onClick={(e) => e.stopPropagation()}>
+          <div className={styles.image_input_container}
+            onMouseEnter={() => setImageSwitch(true)}
+            onMouseLeave={() => setImageSwitch(false)}
+          >
+            <label for="img-input" alt="switch-icon">
+              {image && imageSwitch && (
+                <img
+                  src={require("assets/image-switch.png")}
+                  alt="switch-icon"
+                />
+              )}
+            </label>
+            {image ?
+              <img
+                className={`${styles.image_blur}`}
+                src={URL.createObjectURL(image)}
+                alt="upload"
+              /> :
+              <label for="img-input">
+                <BiImageAdd size="50px" color='black' />
+              </label>
+            }
+            <input
+              name="post_images"
+              id="img-input"
+              type="file"
+              onChange={onSelectFile}
             />
-          )}
-        </label>
-        {image ?
-          <img
-            className={`${styles.image_blur}`}
-            src={URL.createObjectURL(image)}
-            alt="upload"
-          /> :
-          <label for="img-input">
-            <BiImageAdd size="50px" color='black' />
-          </label>
-        }
-        <input
-          name="post_images"
-          id="img-input"
-          type="file"
-          onChange={onSelectFile}
-        />
-      </div>
-      <button>
-        Add Image
-      </button>
-    </div >
+          </div>
+          <button onClick={handleSubmit}>
+            Add Image
+          </button>
+        </div >)
+      }
+    </>
   )
 }
 
