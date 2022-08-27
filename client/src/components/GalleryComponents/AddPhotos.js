e52888d9db04a7524c50eba17373f66e8e746a5c
jayvanandel07 <j.vanandel07@gmail.com>
2022-08-27 11:12:10 +0530

    add functionality

--- client/src/components/GalleryComponents/AddPhotos.js
+++ client/src/components/GalleryComponents/AddPhotos.js
@@ -1,15 +1,56 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { BiImageAdd } from 'react-icons/bi';
 import styles from './AddPhotos.module.css';
 const AddPhotos = () => {
+  const [image, setImage] = useState(undefined);
+  const [imageSwitch, setImageSwitch] = useState(false);
+  const onSelectFile = (e) => {
+    if (!e.target.files || e.target.files.length === 0) {
+      setImage(
+        image || undefined,
+      );
+      return;
+    }
+    setImage(
+      e.target.files[0],
+    );
+  };
+
   return (
     <div className={styles.add_photos_container} onClick={(e) => e.stopPropagation()}>
-      <div className={styles.image_input_container}>
-        add photo
+      <div className={styles.image_input_container}
+        onMouseEnter={() => setImageSwitch(true)}
+        onMouseLeave={() => setImageSwitch(false)}
+      >
+        <label for="img-input" alt="switch-icon">
+          {image && imageSwitch && (
+            <img
+              src={require("assets/image-switch.png")}
+              alt="switch-icon"
+            />
+          )}
+        </label>
+        {image ?
+          <img
+            className={`${styles.image_blur}`}
+            src={URL.createObjectURL(image)}
+            alt="upload"
+          /> :
+          <label for="img-input">
+            <BiImageAdd size="50px" color='black' />
+          </label>
+        }
+        <input
+          name="post_images"
+          id="img-input"
+          type="file"
+          onChange={onSelectFile}
+        />
       </div>
       <button>
         Add Image
       </button>
-    </div>
+    </div >
   )
 }
 
