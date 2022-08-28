fe0c0719090cbd1de8edcec106c8b885a1554481
jayvanandel07 <j.vanandel07@gmail.com>
2022-08-28 15:03:11 +0530

    handle right click

--- client/src/components/MessageComponents/ChatBubble.js
+++ client/src/components/MessageComponents/ChatBubble.js
@@ -1,10 +1,15 @@
-import React from 'react'
+import React, { useState } from 'react'
 import styles from './ChatBubble.module.css'
 function ChatBubble({ type }) {
+  const [isDeleteActive, setIsDeleteActive] = useState(false);
   return (
-    <div className={`${styles.chat_bubble} ${type === 0 && styles.recieve} ${type === 1 && styles.sender}`}>
+    <div className={`${styles.chat_bubble} ${type === 0 && styles.recieve} ${type === 1 && styles.sender}`}
+      onContextMenu={() => setIsDeleteActive(true)}
+    >
       hello lorem
-      <button className={styles.delete_btn}>delete</button>
+      {isDeleteActive &&
+        <button className={styles.delete_btn}>delete</button>
+      }
     </div>
   )
 }
