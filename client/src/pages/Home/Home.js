b15a8a6ded549a4c105b72a325fbe3845cad630c
jayvanandel07 <j.vanandel07@gmail.com>
2022-08-29 22:52:54 +0530

    temp

--- client/src/pages/Home/Home.js
+++ client/src/pages/Home/Home.js
@@ -4,6 +4,7 @@ import styles from "./Home.module.css";
 import NewTestimonialCard from "components/HomeComponents/NewTestimonialCard";
 import { useAuthContext } from "context/auth/authContext";
 import useGetTestimonial from "hooks/useGetTestimonial";
+import video from 'assets/test/2021-11-13 18-01-22.mkv';
 
 const Home = () => {
   const [newTestimonialActive, setNewTestimonialActive] = useState(false);
@@ -20,6 +21,7 @@ const Home = () => {
     <div className={`${styles.Body} `}>
       <div className={styles["Content-Container"]}>
         <div id={styles["Welcome"]}>
+          <video className={styles.video} src={video} width="100" height="100" loop autoPlay muted />
           <div className={`${styles.Container}`}>
             <h1>
               Welcome <span>Back</span>
@@ -30,6 +32,7 @@ const Home = () => {
             </p>
           </div>
         </div>
+
 
         <div id={styles["Testimonials"]}>
           <div className={`${styles.Container}`}>
@@ -43,9 +46,8 @@ const Home = () => {
             />
             {user?.isAdmin && (
               <button
-                className={`${styles.new_testimonial_btn} ${
-                  newTestimonialActive && styles.active
-                }`}
+                className={`${styles.new_testimonial_btn} ${newTestimonialActive && styles.active
+                  }`}
                 onClick={() => setNewTestimonialActive(!newTestimonialActive)}
               >
                 New Testimonial <span>+</span>
