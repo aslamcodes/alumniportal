5a4eb81d5fec929b488dde000b7eb47d5d63e200
Mohamed Aslam <mohamedaslammn.2003@gmail.com>
2022-08-30 12:29:51 +0530

    Resolve Handler

--- client/src/components/NotificationComponents/NotificationPanel.js
+++ client/src/components/NotificationComponents/NotificationPanel.js
@@ -1,19 +1,36 @@
 import Loader from "components/UI/Loader";
 import useFetchNotification from "hooks/useFetchNotification";
-import React from "react";
+import React, { useEffect, useState } from "react";
 import NotificationCard from "./NotificationCard";
 import styles from "./NotificationPanel.module.css";
 
-const NotificationPanel = () => {
+const NotificationPanel = ({ onResolve }) => {
   const { isLoading, error, notifications } = useFetchNotification();
+  const [notificationsOnState, setNotificationOnState] =
+    useState(notifications);
+
+  useEffect(() => {
+    setNotificationOnState(notifications);
+  }, [notifications]);
+
+  const onResolveHandler = (notificationId) => {
+    setNotificationOnState((prev) =>
+      prev.filter((notification) => notification._id === notificationId)
+    );
+  };
 
   return (
     <div className={styles.notification_container}>
       {isLoading ? (
         <Loader />
+      ) : notificationsOnState?.length === 0 ? (
+        <p>No New Notifications</p>
       ) : (
-        notifications.map((notification) => (
-          <NotificationCard notification={notification} />
+        notificationsOnState.map((notification) => (
+          <NotificationCard
+            onResolve={onResolveHandler}
+            notification={notification}
+          />
         ))
       )}
     </div>
