0dfe7789d4ba9d7064adc1a255128507b9c94b83
Mohamed Aslam <mohamedaslammn.2003@gmail.com>
2022-09-06 10:14:05 +0530

    Reset Password Image add

--- client/src/pages/ForgotPassword/ResetPassword.js
+++ client/src/pages/ForgotPassword/ResetPassword.js
@@ -1,5 +1,6 @@
 import React, { useState } from "react";
 import styles from "./ResetPassword.module.css";
+import changePassword from "assets/changePassword.png";
 
 const ResetPassword = () => {
   const [newPassword, setNewPassword] = useState("");
@@ -15,8 +16,14 @@ const ResetPassword = () => {
 
   return (
     <div className={styles.container}>
+      <div className={styles.image_container}>
+        <img src={changePassword} alt="reset password" />
+      </div>
       <div className={styles.form_container}>
         <div className={styles.form}>
+          <div className={styles.form_header}>
+            <h1>Reset Password</h1>
+          </div>
           <form style={{ "align-items": "center" }}>
             <div className={styles.form_input_container}>
               <input
