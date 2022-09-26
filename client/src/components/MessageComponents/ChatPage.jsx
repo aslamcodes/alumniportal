d7d7e68927f06c294c76bb28b0d71710a40c192b
Mohamed Aslam <mohamedaslammn.2003@gmail.com>
2022-09-26 18:59:46 +0530

    Messages Separation

---
+++ client/src/components/MessageComponents/ChatPage.jsx
@@ -0,0 +1,174 @@
+import Loader from "components/UI/Loader";
+import { useAuthContext } from "context/auth/authContext";
+import { useSocketContext } from "context/socket/socketContext";
+import useAxiosWithCallback from "hooks/useAxiosWithCallback";
+import useGetConversationByID from "hooks/useGetConversationByID";
+import { ClientSocketEvents } from "lib/enum";
+import { uniqueId } from "lodash";
+import { useEffect, useRef, useState } from "react";
+import { IoIosArrowBack, IoIosSend } from "react-icons/io";
+import { RiArrowDropDownLine } from "react-icons/ri";
+import { TbLockOpen } from "react-icons/tb";
+import ChatBubble from "./ChatBubble";
+import styles from "./Messages.module.css";
+
+const ChatPage = ({
+  isMessagesActive,
+  messages,
+  onMinimize,
+  isMessagesLoading,
+  onGoBack,
+  conversationId,
+  onSendNewMessage,
+}) => {
+  const [receivedMessages, setReceivedMessages] = useState(messages || []);
+  const [message, setMessage] = useState("");
+  const messagesEndRef = useRef(null);
+
+  const { conversation, isLoading, error } =
+    useGetConversationByID(conversationId);
+
+  const { user } = useAuthContext();
+  const { fetchData: sendMessage } = useAxiosWithCallback();
+  const { socket } = useSocketContext();
+
+  const recipient = conversation?.participants?.filter(
+    (person) => person._id !== user?._id
+  )[0];
+
+  useEffect(() => {
+    socket.on(ClientSocketEvents.RECEIVE_MESSAGE, (data) => {
+      if (recipient?._id === data.senderId) {
+        setReceivedMessages((prev) => [
+          ...prev,
+          {
+            _id: uniqueId(data.message),
+            content: data.message,
+            conversation: conversationId,
+            sender: recipient,
+          },
+        ]);
+      }
+    });
+
+    return () => {
+      socket.off(ClientSocketEvents.RECEIVE_MESSAGE);
+    };
+  }, [socket, setReceivedMessages, recipient, conversationId]);
+
+  useEffect(() => {
+    scrollToBottom();
+  }, [receivedMessages]);
+
+  const scrollToBottom = () => {
+    messagesEndRef.current?.scrollIntoView();
+  };
+
+  const onSubmitHandler = () => {
+    socket.emit(ClientSocketEvents.SEND_MESSAGE, {
+      receiverId: recipient._id,
+      senderId: user?._id,
+      message,
+    });
+
+    const messageConfig = {
+      url: "/api/v1/conversation/message/" + conversationId,
+      method: "post",
+      headers: {
+        Authorization: "Bearer " + user?.token,
+      },
+      data: {
+        content: message,
+      },
+    };
+
+    setReceivedMessages((prev) => [
+      ...prev,
+      {
+        _id: uniqueId(message),
+        content: message,
+        conversation: conversationId,
+        sender: user?._id,
+      },
+    ]);
+
+    sendMessage(messageConfig);
+    onSendNewMessage();
+    setMessage("");
+  };
+
+  if (isLoading) {
+    return <Loader />;
+  }
+
+  return (
+    <>
+      <div className={styles.messages_header}>
+        <div className={styles.chat_profile}>
+          <div>
+            <IoIosArrowBack
+              className={styles.back_btn}
+              fontSize={20}
+              onClick={onGoBack}
+            />
+          </div>
+          <img src={`/api/v1/users/user-avatar/${recipient?._id}`} alt="" />
+          <div>{recipient?.name}</div>
+        </div>
+        <div className={styles.messages_actions}>
+          <RiArrowDropDownLine
+            className={styles.arrow_btn}
+            fontSize={35}
+            onClick={onMinimize}
+          />
+        </div>
+      </div>
+
+      <>
+        <hr className={styles.hr_header} />
+        <div
+          className={`${styles.chat_container} ${styles.chat} ${
+            isMessagesActive && styles.active
+          }`}
+        >
+          <div className={styles.e2e_info}>
+            <h3> Your messages are not e2e encrypted </h3>
+            <TbLockOpen fontSize={17} />
+          </div>
+
+          {isMessagesLoading ? (
+            <Loader />
+          ) : (
+            receivedMessages?.map(({ content, sender }) => {
+              return (
+                <ChatBubble
+                  type={sender === user?._id ? 1 : 0}
+                  message={content}
+                />
+              );
+            })
+          )}
+          <div ref={messagesEndRef} />
+          <div className={styles.input_container}>
+            <span
+              className={styles.textarea}
+              role="textbox"
+              contentEditable={true}
+              suppressContentEditableWarning={true}
+              onBlur={(e) => setMessage(e.currentTarget.textContent)}
+            >
+              {message}
+            </span>
+            <IoIosSend
+              font-size={30}
+              className={styles.send_btn}
+              onClick={onSubmitHandler}
+            />
+          </div>
+        </div>
+      </>
+    </>
+  );
+};
+
+export default ChatPage;
---
+++ client/src/components/MessageComponents/ChatSelectPage.jsx
@@ -0,0 +1,59 @@
+import styles from "./Messages.module.css";
+import { AiOutlinePlus } from "react-icons/ai";
+import { GrFormClose } from "react-icons/gr";
+import Loader from "components/UI/Loader";
+import ChatCard from "./ChatCard";
+
+const ChatSelectPage = ({
+  isConversationsLoading,
+  isMessagesActive,
+  onClose,
+  onChatSelect,
+  conversations,
+}) => {
+  const onChatSelectHandler = (conversationId) => {
+    onChatSelect(conversationId);
+  };
+
+  const onCloseHandler = () => {
+    onClose();
+  };
+
+  return (
+    <>
+      <div className={styles.messages_header}>
+        Messages (alpha)
+        <div className={styles.messages_actions}>
+          <AiOutlinePlus className={styles.add_btn} fontSize={20} />
+          <GrFormClose
+            className={styles.close_btn}
+            fontSize={20}
+            onClick={onCloseHandler}
+          />
+        </div>
+      </div>
+
+      <>
+        <hr className={styles.hr_header} />
+        <div
+          className={`${styles.chat_container} ${
+            isMessagesActive && styles.active
+          }`}
+        >
+          {isConversationsLoading ? (
+            <Loader />
+          ) : (
+            conversations?.map((conversation) => (
+              <ChatCard
+                onSelect={onChatSelectHandler}
+                conversation={conversation}
+              />
+            ))
+          )}
+        </div>
+      </>
+    </>
+  );
+};
+
+export default ChatSelectPage;
