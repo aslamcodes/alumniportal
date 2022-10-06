55a9b52a744a9ab7f7503202e27fda29709efa64
sethupathi <sethupathi@codingmart.com>
2022-10-06 15:58:06 +0530

    Updated index.js file

--- index.js
+++ index.js
@@ -57,11 +57,14 @@ if (process.env.NODE_ENV === "DEVELOPMENT") {
   });
 } else {
   console.log("Production mode".green);
-  const root = path.join(__dirname, "client", "build");
-  app.use(express.static(root));
-  app.get("*", (req, res) => {
-    res.sendFile("index.html", { root });
+  app.get("/", (req, res) => {
+    res.send("ALUMNI PORTAL API RUNNING");
   });
+  // const root = path.join(__dirname, "client", "build");
+  // app.use(express.static(root));
+  // app.get("*", (req, res) => {
+  //   res.sendFile("index.html", { root });
+  // });
 }
 
 app.use(notFound);
