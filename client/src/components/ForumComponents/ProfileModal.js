1d7602856bb41ea33d09cd7a3252e0da7509be31
Heemanush <cph2k2@gmail.com>
2022-10-12 15:12:20 +0530

    testing

--- client/src/components/ForumComponents/ProfileModal.js
+++ client/src/components/ForumComponents/ProfileModal.js
@@ -58,7 +58,7 @@ function ProfileModal({ isOpen, handleClose, userId }) {
   const { successAlert, errorAlert } = useAlertContext();
 
   useEffect(() => {
-    document.title = "Alumni Portal | Profile";
+    document.title = "Alumni Por | Profile";
   }, []);
 
   const pick_image = useCallback(() => {
