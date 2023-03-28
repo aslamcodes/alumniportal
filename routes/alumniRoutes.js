092452be8c9da58ec8d89767aea0710762156f6e
Mohamed Aslam <mohamedaslammn.2003@gmail.com>
2023-03-28 10:43:21 +0530

    Backend

--- routes/alumniRoutes.js
+++ routes/alumniRoutes.js
@@ -16,6 +16,7 @@ import {
   getAllAlumniV2,
   getSearchAlumniPartial,
   getSearchAlumniFull,
+  generateAlumniPDF,
 } from "../controllers/alumniControllers.js";
 import { admin, protect } from "../middleware/authMiddlewares.js";
 
@@ -35,6 +36,7 @@ router.post("/register", registerAlumni);
 
 router.patch("/:id", protect, updateAlumni);
 router.patch("/approve/:id", protect, admin, approveAlumni);
+router.post("/generate/:id", protect, admin, generateAlumniPDF);
 router.patch("/reject/:requestId", protect, admin, rejectAlumniRequest);
 router.patch("/set-office-bearer/:id", protect, admin, setOfficeBearer);
 router.patch("/remove-office-bearer/:id", protect, admin, removeOfficeBearer);
